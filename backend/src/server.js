455b4388a0de9a8886065c72d388ef741b4134a3
IT24100103 <it24100103@my.sliit.lk>
2026-03-24 01:17:23 +0530

    Fix Express 5 catch-all route with named wildcard (*all)

--- backend/src/server.js
+++ backend/src/server.js
@@ -69,7 +69,7 @@ app.use((req, res, next) => {
 });
 
 // Catch-all for React Router
-app.get('(.*)', (req, res) => {
+app.get('*all', (req, res) => {
   res.sendFile(path.resolve(frontendPath, 'index.html'));
 });
 
