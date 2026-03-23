2404fe5f1915382384d6dc7740ba7e7a173f9117
IT24100103 <it24100103@my.sliit.lk>
2026-03-24 01:12:24 +0530

    Fix Express 5 catch-all route syntax

--- backend/src/server.js
+++ backend/src/server.js
@@ -69,7 +69,7 @@ app.use((req, res, next) => {
 });
 
 // Catch-all for React Router
-app.get('*', (req, res) => {
+app.get('(.*)', (req, res) => {
   res.sendFile(path.resolve(frontendPath, 'index.html'));
 });
 
