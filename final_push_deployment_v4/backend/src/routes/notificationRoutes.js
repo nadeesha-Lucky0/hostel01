407a2211094ef86c7b44b90ea34be0c059b38f49
IT24100103 <it24100103@my.sliit.lk>
2026-03-31 21:49:35 +0530

    Final Fix for Express 5 compatibility on Render (Resolved PathError)

--- final_push_deployment_v4/backend/src/routes/notificationRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { authenticate, authorize } = require("../middleware/auth");
-const { getMyNotifications, clearMyNotifications } = require("../controllers/notificationController");
-
-router.get("/me", authenticate, authorize("student"), getMyNotifications);
-router.delete("/me", authenticate, authorize("student"), clearMyNotifications);
-
-module.exports = router;
